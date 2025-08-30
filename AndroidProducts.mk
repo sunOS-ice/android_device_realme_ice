200faeff61196980b93fef58b1b16a273bcff703
Debarpan102 <debarpan102github@gmail.com>
2025-08-30 18:25:16 +0530

    ice: Bringup sunOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_ice.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ice-user \
-    lineage_ice-userdebug \
-    lineage_ice-eng
+    sun_ice-user \
+    sun_ice-userdebug \
+    sun_ice-eng
